0923b64e25966266478f3ab7a195ff8609d17db1
praniil <pparajuli15@gmail.com>
2022-06-24 08:10:48 +0545

    new commit

---
+++ casechange.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char a ;
+    printf("input a character :");
+    scanf("%c", &a);
+    if(a>67 && a<90){
+        a= a + 32 ;
+        printf("the lower case is :%c", a);
+    }
+    else{
+        a= a-32 ;
+        printf("the upper case is :%c", a);
+    }
+return 0;
+}
