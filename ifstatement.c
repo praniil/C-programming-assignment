d8bedfd0de2fd261f26f3ee7994970ce6e3cce06
praniil <pparajuli15@gmail.com>
2022-06-25 17:05:41 +0545

    checking if the number is greater than 5 or 10 and if the number is smaller than 100

---
+++ ifstatement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int number;
+    printf("enter a number");
+    scanf("%d", &number);
+    if(number>5){
+        printf("the number is greater than 5 \n");
+        }
+        if(number>10){
+            printf("the number is greater than 10\n");
+        }
+        if(number<100){
+            printf("the number is smaller than 100 \n");
+        }
+    }
+
+    
